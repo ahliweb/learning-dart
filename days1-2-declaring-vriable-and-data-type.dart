62c64a3d555aa84f1950682a3388174524d1c328
Unggul <indosaputra@gmail.com>
2024-05-22 23:09:35 +0700

    days1-2

---
+++ days1-2-declaring-vriable-and-data-type.dart
@@ -0,0 +1,11 @@
+void main() {
+  var name = 'Alice';
+  int age = 30;
+  double height = 5.6;
+  bool isStudent = false;
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: $height');
+  print('Is Student: $isStudent');
+}
--- hello.dart
+++ hello.dart
@@ -1,7 +1,3 @@
 void main() {
   print('hello');
 }
-
-var name = 'voyager';
-var year = 1999;
-var antennaDiameter = 5.7;
